edc2908e90c991db3102576ff45be84589a0f281
ArturoTorresMartinez <efrainarturotorres@gmail.com>
2018-09-10 17:48:54 -0500

    High Database Personal MLab Functionality

--- Arturo/Mean-Tests/BackEnd/database.js
+++ Arturo/Mean-Tests/BackEnd/database.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const URI = 'mongodb://localhost/mean-crud';
+//const URI = 'mongodb://localhost/mean-crud';
+const URI = 'mongodb://root:poq7283@ds149682.mlab.com:49682/consultoria';
 
 //Conection to database
 mongoose.connect(URI,  {useNewUrlParser: true})
